ddec7b2475fa6d4da5f55a46a3a83f2a6ec1cf0f
Zhirui <jerrio20@outlook.com>
2019-06-24 18:02:17 +0800

    add index dynamic navigator

--- TheShortFilmShow/unpackage/dist/dev/app-plus/pages/register/register.js
+++
@@ -1,2 +0,0 @@
-setCssToHead(["body { width: 100%; height: 100%; }\n.",[1],"Reg-back { height: 100%; background-color: white; }\n.",[1],"logo { display: -webkit-box; display: -webkit-flex; display: -ms-flexbox; display: flex; width: 100%; }\n.",[1],"itemView { height: 10%; border: ",[0,1]," solid black; -webkit-border-radius: 2px; border-radius: 2px; background-color: #F8F8FF; }\n",],undefined,{path:"./pages/register/register.wxss"})();
-document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/register/register.wxml') } }));
