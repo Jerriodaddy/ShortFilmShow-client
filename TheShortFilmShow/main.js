76eeb78d32946a8aca720eb95b68ce51c40a1598
Zhirui <jerrio20@outlook.com>
2019-06-25 14:11:45 +0800

    waiting to merge all

---
+++ TheShortFilmShow/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+Vue.prototype.$serverUrl = 'http://127.0.0.1:8080'
+
+Vue.prototype.getGlobalUserInfo = function(user) {
+	uni.setStorageSync('userInfo', user);
+}
+
+Vue.prototype.getGlobalUserInfo = function() {
+	return uni.getStorageSync('userInfo');
+}
+
+const app = new Vue({
+	...App
+})
+app.$mount()
